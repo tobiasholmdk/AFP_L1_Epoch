763c86a2b313fdeb65014b93b04b7400d4f1abe7
Tobias Holm <201608355@post.au.dk>
2021-02-02 14:24:51 +0100

    Added exercises 1.2-1.5 for L1 of AFP

---
+++ Epoch-Day/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+// Opgave 1.2
+let isLeap (year:int)=
+    if ( year % 400 = 0 && year % 100 <> 0 || year % 4 = 0 && year % 100 <> 0 ) then true
+    else false
+    
+// Opgave 1.3
+let daysToEndYear (year:int) =
+    let LeapYears = List.filter isLeap [1970..year]
+    ((year-1970)*365) + (List.length LeapYears)
+  
+// Opgave 1.4    
+let daysToEndMonth (month, year) =
+    let correction = match month with
+                     | 1 -> 0
+                     | _ when isLeap year -> 1
+                     | _ -> 2
+    (367*month+5)/12 - correction + daysToEndYear (year-1)
+
+// Opgave 1.5 
+let epochDay(day, month, year) =
+    day + daysToEndMonth (month, year)              
+                     
+[<EntryPoint>]
+let main argv =
+    while true do
+        let result = epochDay( Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()));
+        Console.WriteLine( result );
+    0 // return an integer exit code
